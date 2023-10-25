7fd648424abc4ac70f1dc43c57dbbbd69023b87a
木子丶小二 <46108588+suhxhee@users.noreply.github.com>
2023-10-25 23:16:59 +0800

    Add files via upload

---
+++ object.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+enum 人类性别
+{
+    男,
+    女,
+    其他
+};
+struct 人类
+{
+    string 姓名;
+    人类性别 性别;
+    unsigned int 年龄;
+    unsigned int 身高;
+    unsigned int 体重;
+    unsigned int 三围;
+    
+};
+int main(void)
+{
+    人类 *对象 = new 人类;
+    对象->姓名 = "宇沢レイサ";
+    对象->性别 = 女;
+
+    return 0;
+}
