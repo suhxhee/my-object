93c757634c54322e54cd3c68867c3d104754470b
木子丶小二 <46108588+suhxhee@users.noreply.github.com>
2023-10-26 00:24:17 +0800

    Update object.cpp

--- object.cpp
+++ object.cpp
@@ -10,14 +10,12 @@ struct 人类
 {
     string 姓名;
     人类性别 性别;
-    unsigned int 年龄;
-    unsigned int 身高;
-    unsigned int 体重;
-    unsigned int 三围;
+    unsigned int 年龄, 身高, 体重, 三围;
     
 };
 int main(void)
 {
+    //具体例子
     人类 *对象 = new 人类;
     对象->姓名 = "宇沢レイサ";
     对象->性别 = 女;
